078c4ad16d16d0136e649b1172abe6dab0ca40f1
Quinn Bellmore <93296277+quinnzbellz@users.noreply.github.com>
2022-05-19 16:54:34 -0400

    Added spaces back once shown again

--- show.sh
+++ show.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 cd ~/Desktop
+for file in .*; do mv -f -v "$file" "$(echo $file | sed 's/_/ /g')" ; done
 for f in .*; do
     if [ "$f" != . -a "$f" != .. ]; then
         mv "$f" "${f:1}"
